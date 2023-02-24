abb603bedd49f10dbdc8e199f71756e85ccb1e33
Anton Yanyushkin <scamii@yandex.ru>
2023-02-24 14:36:39 +0500

    изучаю theme.json

--- function.php
+++ function.php
@@ -1,21 +1,43 @@
 <?php
 
-if ( ! function_exists( 'my_theme_name_support' ) ) :
+add_action( 'after_setup_theme', 'paperless_theme_support' );
+add_action( 'wp_enqueue_scripts', 'paperless_styles' );
+
+
+if ( ! function_exists( 'paperless_theme_support' ) ) :
 
 	/**
 	 * Sets up theme defaults and registers support for various WordPress features.
 	 *
 	 * @since My theme name 1.0
+	 *
 	 * @return void
 	 */
-	function my_theme_name_support(): void {
 
-		// Add support for block styles.
-		add_theme_support( 'wp-block-styles' );
+	function paperless_theme_support() {
 
-		// Enqueue editor styles.
-		add_editor_style( 'style.css' );
+		/**
+		 * Add support from style.
+		 */
+
+		add_theme_support( 'wp-block-style' );
+
+		/**
+		 * Enqueue editor styles.
+		 */
+
+		 add_editor_style( 'style.css' );
 	}
 
-endif;
-add_action( 'after_setup_theme', 'my_theme_name_support' );
+	endif;
+
+	/**
+	 * Enqueue Styles.
+	 */
+
+function paperless_styles() {
+
+	wp_enqueue_style( 'paperless-style', get_stylesheet_uri() );
+	wp_enqueue_style( 'paperless-style-blocks', get_template_directory_uri() . '/assets/css/blocks.css' );
+
+}
